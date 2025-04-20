5490927ed5198af85bca5d58018cb3beb56df0cf
Karthikkola <karthikkola@icloud.com>
2025-04-20 17:44:55 -0400

    Add Notify page component

---
+++ src/pages/Notify.tsx
@@ -0,0 +1,8 @@
+export default function Notify() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold text-gray-900">Notify</h1>
+      <p className="mt-2 text-gray-600">Notification management will be displayed here.</p>
+    </div>
+  );
+} 
